99b615395650fdc0b9d9203f528635d9dbfc4d61
Doc94 <nachito94@msn.com>
2015-03-18 18:21:16 -0300

    Change

--- data/config.php
+++ data/config.php
@@ -8,8 +8,8 @@
  
 /* Configuracion de conexion query hacia TS3 */
 
-$HOST_QUERY = "131.221.33.40"; //HOST:QUERY_PORT Que usa tu servidor de ts3
-$PORT_QUERY = "10011";
+$HOST_QUERY = "IP"; //HOST:QUERY_PORT Que usa tu servidor de ts3
+$PORT_QUERY = "10011"; //Default Query
 $USER_QUERY = "USER"; //Usuario de conexion a query
 $PASS_QUERY = "MICLAVE"; //Clave generada para a conexion query
 $SERVER_PORT = "9977"; //Puerto del servidor ts3
