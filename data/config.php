9a7116fed13bf977a510dcb6404bf19711cf3488
DmACKGL <fsanllehi@gamerlive.cl>
2018-02-10 06:36:44 -0300

    Desactivar debug por default

--- data/config.php
+++ data/config.php
@@ -11,8 +11,8 @@ $script_version = "V1.5";
 /* Configuracion de conexion query hacia TS3 */
 $HOST_QUERY = "localhost"; //HOST:QUERY_PORT Que usa tu servidor de ts3
 $PORT_QUERY = "10011"; //Puerto de query por default es 10011
-$USER_QUERY = "serveradmin"; //Usuario de conexion a query
-$PASS_QUERY = "hAsdzIXD"; //Clave generada para a conexion query
+$USER_QUERY = "QUERYUSER"; //Usuario de conexion a query
+$PASS_QUERY = "QUERYPASS"; //Clave generada para a conexion query
 $SERVER_PORT = "9987"; //Puerto del servidor ts3
 
 /* Configuracion de variables Front-end */
@@ -21,4 +21,4 @@ $IDIOMA = "ES"; // (Options: en, es) Idioma de ejecucion
 $SID_GROUP = array(75); //SORT_ID del grupo que sera listado Example array(1,2,3,4,5)
 $NICK_QUERY = "AdminSystem"; //Nickname de la conexion query
 $MAX_ICONS = 8; //Maximo de iconos seleccionar
-$DEBUG = True; //Activa los mensajes de error detallados
+$DEBUG = false; //Activa los mensajes de error detallados
